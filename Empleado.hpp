9cbef6f93151c34d220e8e0db7ae69c31b058e1b
Pablo Infantes Soto <pablo.infantes@ucsp.edu.pe>
2025-05-28 08:14:27 -0500

    Update Empleado.hpp

--- Empleado.hpp
+++ Empleado.hpp
@@ -2,6 +2,21 @@
 #define EMPLEADO_HPP
 
 #include "Persona.hpp"
+
+class Empleado : public Persona {
+public:
+    char puesto[50]; //Jefe,Cajero,etc
+    double salario; //Dinero
+
+public:
+    Empleado(const char* nombre, int edad, const char* dni, const char* puesto, double salario);
+    void mostrarInfo() override;
+};
+
+#endif
+
+/*
+#include "Persona.hpp"
 class Empleado{
 
 public: //private proteger su informacion
@@ -20,4 +35,4 @@ gerente(funcion acceso a todo) :v
 empleado(horariotrabajo,paga,trabajo)
 trabajar=paga
 }
-**/
+*/
