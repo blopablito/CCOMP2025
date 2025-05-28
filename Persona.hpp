dda14466928f010188926c0e477b9c4781dc8c09
Pablo Infantes Soto <pablo.infantes@ucsp.edu.pe>
2025-05-28 07:41:56 -0500

    Create Persona.hpp

---
+++ Persona.hpp
@@ -0,0 +1,18 @@
+#ifndef PERSONA_HPP
+#define PERSONA_HPP
+
+class Persona {
+public: //CREO que deberia ser protecte ya se vera
+    char nombre[50];
+    int edad;
+    char dni[8];
+
+public:
+    Persona(const char* nombre, int edad, const char* dni);
+    virtual void mostrarInfo();
+};
+
+#endif // PERSONA_HPP
+
+
+#endif
