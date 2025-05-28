11db343d201edccabacd2500005b80e5a4f5f266
abelperez01 <abel.perez@ucsp.edu.pe>
2025-05-28 08:20:39 -0500

    Update Producto.hpp

--- Producto.hpp
+++ Producto.hpp
@@ -1,2 +1,10 @@
 #ifndef PRODUCTO_HPP
 #define PRODUCTO_HPP
+class Producto {
+private:
+    std::string nombre;
+    int cantidad;
+    double preciouniddad;
+
+public:
+    Producto(std::string nombre, int cantidad, float preciounidad);
