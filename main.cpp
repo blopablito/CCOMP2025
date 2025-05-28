1757a2da3642e044cb7b3696a7a7f778f808b92a
abelperez01 <abel.perez@ucsp.edu.pe>
2025-05-28 08:07:33 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 
 int main() {
-    Cliente cliente1("Juan Perez", 30, "12345678", "Comida vegetariana");
-    Empleado empleado1("Ana Garcia", 25, "87654321", "Camarera", 1200);
+    Cliente cliente1("Arroe Calle", 30, "12345678", "Comida noxiana");
+    Empleado empleado1("Darius", 25, "87654321", "Camarera", 1200);
 
     cout << "Información del Cliente:" << endl;
     cliente1.mostrarInfo();
