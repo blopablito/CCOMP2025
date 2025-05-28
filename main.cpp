7b0be59f86f15345cfbec174baacff438bbd531e
Pablo Infantes Soto <pablo.infantes@ucsp.edu.pe>
2025-05-28 08:06:31 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "Cliente.hpp"
+#include "Empleado.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    Cliente cliente1("Juan Perez", 30, "12345678", "Comida vegetariana");
+    Empleado empleado1("Ana Garcia", 25, "87654321", "Camarera", 1200);
+
+    cout << "Información del Cliente:" << endl;
+    cliente1.mostrarInfo();
+
+    cout << "\nInformación del Empleado:" << endl;
+    empleado1.mostrarInfo();
+
+    return 0;
+}
