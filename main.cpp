1b6dc61e1e4e5d7d6d33012d9633e318f1727f95
Pablo Infantes Soto <pablo.infantes@ucsp.edu.pe>
2025-05-28 08:19:35 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
+#include "Cliente.hpp"
 #include "Empleado.hpp"
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    Empleado empleado1("Darius", 25, "87654321", "Camarera", 1200);
+    Cliente cliente1("Juan Perez", 30, "12345678", "Pizza");
+    Empleado empleado1("Ana Garcia", 25, "87654321", "Camarera", 1200);
+
+    cout << "Información del Cliente:" << endl;
+    cliente1.mostrarInfo();
 
     cout << "\nInformación del Empleado:" << endl;
     empleado1.mostrarInfo();
