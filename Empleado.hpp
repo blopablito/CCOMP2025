2e6d9abfe1c2743309d225af0f4879188f949dfe
Pablo Infantes Soto <pablo.infantes@ucsp.edu.pe>
2025-05-28 08:02:59 -0500

    Update Empleado.hpp

--- Empleado.hpp
+++ Empleado.hpp
@@ -1,1 +1,22 @@
-a
+#ifndef EMPLEADO_HPP
+#define EMPLEADO_HPP
+
+#include "Persona.hpp"
+class Empleado{
+
+public: //private proteger su informacion
+  int HorarioDeTrabajo;
+  int HorasTrabajo;
+  double PagaporTrabajo;
+
+public:
+  int trabajar(int hora);
+  float paga();
+};
+/**
+int main (){
+gerente(funcion acceso a todo) :v
+empleado(horariotrabajo,paga,trabajo)
+trabajar=paga
+}
+**/
