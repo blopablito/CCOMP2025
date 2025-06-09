905b3a5190e42b72178716e859cb2a980c713ab9
abelperez01 <abel.perez@ucsp.edu.pe>
2025-06-09 13:55:55 -0500

    Update Inventario.hpp

--- Inventario.hpp
+++ Inventario.hpp
@@ -2,6 +2,16 @@
 #define INVENTARIO_HPP
 
 class inventario{
-public;
-//producto
-//cantidad producto
+public:   //cantidad producto
+    Inventario(const char* producto, int cantidad);
+    ~Inventario();
+    void mostrarInventario();
+    void agregarCantidad(int extra);
+    void reducirCantidad(int menos);
+private:   //producto
+    char producto[50];
+    int cantidad;
+
+};
+
+#endif
